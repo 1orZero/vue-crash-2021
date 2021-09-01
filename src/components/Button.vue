532031897fe54bf46bff15891f13cd93407f37df
KaMeng <laokameng.6@gmail.com>
2021-09-01 08:21:25 +0800

    DONE

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+  <button
+    @click="onClick()"
+    class="btn"
+    :style="{ background: color }"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: {
+      type: String,
+      default: "Default",
+    },
+    color: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("btn-click");
+    },
+  },
+};
+</script>
+
+    
